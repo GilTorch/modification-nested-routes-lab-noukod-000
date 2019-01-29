fc19addbffab25be8de8ba6f1b0268a1209459f0
Gilbert Torchon <gilbert@noukod.com>
2019-01-29 21:49:12 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,7 +45,7 @@ class SongsController < ApplicationController
   def edit
    if params[:artist_id]
      @artist = Artist.find_by(id: params[:artist_id])
-   if artist.nil?
+   if @artist.nil?
      redirect_to authors_path, alert: "Artist not found."
    else
      @song = @artist.posts.find_by(id: params[:id])
